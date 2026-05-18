3d5d7ec527e22e2149574b64965e8d187dee23c7
Oyin414 <kadirioyin88@gmail.com>
2026-05-19 00:19:57 +0100

    feat: add head, tail, prepend, size, at, pop, contains, findIndex

--- src/linkedList.js
+++ src/linkedList.js
@@ -29,4 +29,107 @@ export class LinkedList {
     }
     return (list += `( ${currentNode.value} ) -> ( null )`);
   }
+
+  head() {
+    if (this.headNode === null) {
+      return undefined;
+    }
+    let currentNode = this.headNode;
+    let name = currentNode.value;
+    return name;
+  }
+
+  tail() {
+    if (this.headNode === null) {
+      return undefined;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null) {
+      currentNode = currentNode.nextNode;
+    }
+    return currentNode.value;
+  }
+
+  prepend(value) {
+    if (this.headNode === null) {
+      this.headNode = new Node(value, null);
+      return;
+    }
+    let currentNode = this.headNode;
+    this.headNode = new Node(value, null);
+    return (this.headNode.nextNode = currentNode);
+  }
+
+  pop() {
+    if (this.headNode === null) {
+      return undefined;
+    }
+    let currentNode = this.headNode;
+    if (currentNode.nextNode === null) {
+      this.headNode = null;
+    } else {
+      this.headNode = currentNode.nextNode;
+    }
+    return currentNode.value;
+  }
+
+  contains(value) {
+    if (this.headNode === null) {
+      return false;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null && currentNode.value !== value) {
+      currentNode = currentNode.nextNode;
+    }
+    if (currentNode.value !== value) {
+      return false;
+    }
+    return true;
+  }
+
+  at(index) {
+    let item;
+    let number = 0;
+    if (this.headNode === null) {
+      return undefined;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null && number < index) {
+      currentNode = currentNode.nextNode;
+      number += 1;
+    }
+    if (index > number) {
+      return undefined;
+    }
+    return (item = currentNode.value);
+  }
+
+  findIndex(value) {
+    let index = 0;
+    if (this.headNode === null) {
+      return -1;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null && currentNode.value !== value) {
+      currentNode = currentNode.nextNode;
+      index += 1;
+    }
+    if (currentNode.value !== value) {
+      return -1;
+    }
+    return index;
+  }
+
+  size() {
+    let number = 0;
+    if (this.headNode === null) {
+      return number;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null) {
+      number += 1;
+      currentNode = currentNode.nextNode;
+    }
+    return (number += 1);
+  }
 }
