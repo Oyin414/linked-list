0bc7a020977b97daad632b0b14e34172c666814d
Oyin414 <kadirioyin88@gmail.com>
2026-05-19 00:17:18 +0100

    feat:add appeend and toString methods

--- src/linkedList.js
+++ src/linkedList.js
@@ -4,4 +4,29 @@ export class LinkedList {
   constructor() {
     this.headNode = null;
   }
+
+  append(value) {
+    if (this.headNode === null) {
+      this.headNode = new Node(value, null);
+      return;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null) {
+      currentNode = currentNode.nextNode;
+    }
+    currentNode.nextNode = new Node(value, null);
+  }
+
+  toString() {
+    let list = "";
+    if (this.headNode === null) {
+      return list;
+    }
+    let currentNode = this.headNode;
+    while (currentNode.nextNode !== null) {
+      list += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.nextNode;
+    }
+    return (list += `( ${currentNode.value} ) -> ( null )`);
+  }
 }
